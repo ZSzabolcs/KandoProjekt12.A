1a73f33e7097819b9fd142bf0068872d310f1b9f
Cs12krisz <90339656+Cs12krisz@users.noreply.github.com>
2024-11-14 12:24:27 +0100

    Add files via upload

--- Projektmunka 12.A/Developer_class.php
+++ Projektmunka 12.A/Developer_class.php
@@ -4,18 +4,18 @@ use PDOException;
 use PDO;
 final class DeveloperDB
 {
-    private static string $servername;
-    private static string $db_username;
-    private static string $db_password;
-    private static string $db_name;
-    private static string $PDO_name = "sqlite:Blogger.db";
+    private static  $servername;
+    private static  $db_username;
+    private static $db_password;
+    private static $db_name;
+    private static $PDO_name = "sqlite:Blogger.db";
     public const FETCH_ASSOC = PDO::FETCH_ASSOC;
     public const PARAM_STR = PDO::PARAM_STR;
 
     public static function CallPDO()
     {
         try{
-        $db = new PDO(dsn: self::$PDO_name);
+        $db = new PDO(self::$PDO_name);
         $db->exec('PRAGMA foreign_keys = ON;');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
